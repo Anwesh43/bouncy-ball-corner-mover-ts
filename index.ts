7f0f894904c1e2c7ab1df1860ea5ff2899850765
Anwesh43 <anweshthecool0@gmail.com>
2020-03-21 15:11:24 +0530

    adding BouncyBallCornerMover class

--- index.ts
+++ index.ts
@@ -179,3 +179,27 @@ class BBCMNode {
         return this
     }
 }
+
+class BouncyBallCornerMover {
+
+    root : BBCMNode = new BBCMNode(0)
+    curr : BBCMNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
