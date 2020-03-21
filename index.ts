5fe95eedceb4b2f471836719a12c5a803bcabcc8
Anwesh43 <anweshthecool0@gmail.com>
2020-03-21 15:35:31 +0530

    adding fixes

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 const w : number = window.innerWidth
 const h : number = window.innerHeight
-const scGap : number = 0.02
+const corners : number = 4
+const scGap : number = 0.02 / corners
 const strokeFactor : number = 90
 const sizeFactor : number = 2.9
-const rFactor : number = 30
-const delay : number = 30
+const rFactor : number = 6
+const delay : number = 15
 const nodes : number = 5
-const corners : number = 4
 const foreColor : string = "#2196f3"
 const backColor : string = "#BDBDBD"
 
@@ -27,26 +27,34 @@ class ScaleUtil {
 
 class DrawingUtil {
 
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
     static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
         context.beginPath()
         context.arc(x, y, r, 0, 2 * Math.PI)
         context.fill()
     }
 
-    static drawBouncyCornerBall(context : CanvasRenderingContext2D, i : number, scale : number, size : number) {
+    static drawBouncyCornerBall(context : CanvasRenderingContext2D, scale : number, size : number) {
+        const scDiv : number = 1 / corners
+        const i : number = Math.floor(scale / scDiv)
         const sc : number = ScaleUtil.divideScale(scale, i, corners)
         const sf : number = ScaleUtil.sinify(sc)
         const r : number = size / rFactor
+        const x : number = size * sf
+        context.lineCap = 'round'
+        context.lineWidth = r
+        context.strokeStyle = foreColor
         context.save()
         context.rotate(i * Math.PI / 2)
-        DrawingUtil.drawCircle(context, size * sc, 0, r)
+        DrawingUtil.drawLine(context, 0, 0, x, 0)
+        DrawingUtil.drawCircle(context, x, 0, r)
         context.restore()
-    }
-
-    static drawBouncyCornerBalls(context : CanvasRenderingContext2D, scale : number, size : number) {
-        for (var i = 0; i < corners; i++) {
-            DrawingUtil.drawBouncyCornerBall(context, i, scale, size)
-        }
     }
 
     static drawBBCMNode(context : CanvasRenderingContext2D, i : number, scale : number) {
@@ -55,7 +63,7 @@ class DrawingUtil {
         const size : number = gap / sizeFactor
         context.save()
         context.translate(w / 2, gap * (i + 1))
-        DrawingUtil.drawBouncyCornerBalls(context, scale, size)
+        DrawingUtil.drawBouncyCornerBall(context, scale, size)
         context.restore()
     }
 }
